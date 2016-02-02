2c1b263e84254275a31d182e3b7fe904b7d3c14d
fivef <fivef2@gmail.com>
2016-02-02 20:33:36 +0100

    before speed stuff

--- generate_trajectory.m
+++ generate_trajectory.m
@@ -61,16 +61,20 @@ path = [0.57 0.1 0; 0.30 0.1 0; 0.57 0.1 0; 0.57 0.1 0; 0.30 0.1 0; 0.57 0.1 0];
 %plot3(path(:,1), path(:,2), path(:,3), 'color', 'k', 'LineWidth', 2)
 
 % x y z speedlimit, empty, xyz initial position, timestep, time acceleration is
-% applied (strange parameter as smaller values give higher acc/speed?)
+% applied to segment transition
 %p = 
 %mstraj(path, [2000 2000 2000], [], [0 0 0], 0.1, 0.9)
 
 timestep = 0.1
 
+
 %plots the trajectory
-mstraj(path, [0.1 0.1 0.1], [], [path(1,1) path(1,2) 0], timestep, 0.1)
+%mstraj(path, [0.1 0.1 0.1], [], [path(1,1) path(1,2) 0], timestep, 0.1)
+
+
 
 p = mstraj(path, [0.1 0.1 0.1], [], [path(1,1) path(1,2) 0], timestep, 0.1)
+
 
 %scale the trajectory
 Tp = transl(p);
@@ -88,12 +92,28 @@ disp(['Calculating IK...'])
 
 %calc ikine for the trajectory, start point,  mask which dofs to use/ignore
 %q_out = twolink.ikine(Tp,[0 0], [1 1 0 0 0 0], 'plot')
-[q_out,err,exitflag] = twolink.ikcon(Tp)
+%[q_out,err,exitflag] = twolink.ikcon(Tp)
 
 
 %simulate the trajectory
 %twolink.plot(q_out)
 
+
+
+
+
+total_duration_in_s = 2.0
+steps = 100
+t = 0:total_duration_in_s/steps:total_duration_in_s
+
+%directly generate 2 point trajectory:
+q0 = [0 0]
+qf = [1 0]
+
+mtraj(@tpoly, q0, qf, steps)
+[q, qd, qdd] = mtraj(@tpoly, q0, qf, t)
+
+q_out = q
 
 %get rid of first value
 q_out = q_out(2:end,:)
@@ -102,9 +122,7 @@ q_out = q_out(2:end,:)
 q_out(:,1) = radians_to_encoder_steps(q_out(:,1),JOINT_1_TRANSMISSION)
 q_out(:,2) = radians_to_encoder_steps(q_out(:,2),JOINT_2_TRANSMISSION)
 
-total_duration_in_s = 10.0
 
-t = 0:total_duration_in_s/length(q_out):total_duration_in_s
 t = t(2:end)'
 wave1.signals.values = [q_out(:,1)]
 wave1.time = t
